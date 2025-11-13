04c8a32430036cb21b9d9fff48e03b33d9aaf90d
SoumyaPandua <138252829+SoumyaPandua@users.noreply.github.com>
2025-11-13 12:19:52 +0530

    Update API response message for root endpoint

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
-  res.send('API is running...');
+  res.send('API is running on EC2...');
 });
 
 export default app;
