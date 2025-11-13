46fcff1e4c5bb7fd508dd5addb03ee8f82a60228
SoumyaPandua <138252829+SoumyaPandua@users.noreply.github.com>
2025-11-13 12:55:50 +0530

    test auto deploy

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
-  res.send('API is running on EC2...');
+  res.send('API is running on EC2 using CI/CD pipeline...');
 });
 
 export default app;
